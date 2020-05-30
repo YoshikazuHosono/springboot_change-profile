b131d110280f38618d1ceb22467509c19c5c5b17
YoshikazuHosono <yyhhwork@gmail.com>
2020-05-30 12:05:10 +0900

    add @Profile sample

--- src/main/java/com/example/demo/schedule/DemoScheduler.java
+++ src/main/java/com/example/demo/schedule/DemoScheduler.java
@@ -1,17 +1,19 @@
 package com.example.demo.schedule;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("pro")
 public class DemoScheduler {
 
     @Value("${myprofile.name}")
     private String profileName;
 
     @Scheduled(fixedDelay = 1000)
-    public void demoTask(){
+    public void demoTask() {
         System.out.println("run demo task on " + profileName);
     }
 
